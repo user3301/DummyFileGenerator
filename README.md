b9437cb3f7ffd03d34f0698b0fa849310e0c574b
user3301 <stan_gai@Hotmail.com>
2018-04-15 23:01:41 +1000

    wip

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # DummyFileGenerator
 
-[![license](https://img.shields.io/github/license/slashsBin/styleguide-git-commit-message.svg)](https://github.com/user3301/DummyFileGenerator/blob/master/LICENSE)
+[![license](https://img.shields.io/github/license/slashsBin/styleguide-git-commit-message.svg)](https://github.com/user3301/DummyFileGenerator/blob/master/LICENSE)<br/>
 DummyFileGenerator generates dummy files with arbitrary file size for testing purpose.
--- program.py
+++ program.py
@@ -5,9 +5,15 @@ numberOfFiles = 100
 outputDir = 'C:\Users\Zhelun Gai\desktop'
 
 # create directory
-def createDirectory(self, output_dir):
+def createDirectory(output_dir):
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
 # create a file in specified directory
-def createFile(self, file_type, output_dir):
+def createFile(file_type, output_dir):
+    with open(os.path.join(output_dir, filename), 'w') as dummyFile:
+        dummyFile.write(os.urandom((1024)))
+
+# generate non-repeat file name
+def generateFileName():
+    return 'dummy_file_%s.%s'%
