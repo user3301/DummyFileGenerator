581bbc478443c984548ada84cedb87dd79d85b4e
CJ\zhelun <zhelun.gai@tmacomms.com>
2018-04-16 12:00:13 +1000

    methods for generating dummy files

--- lib/core.py
+++ lib/core.py
@@ -2,23 +2,32 @@ import os
 import random
 import string
 import time
-from random import randint
+import configparser
+from threading import Thread
 
-fileType = 'mp3'
-numberOfFiles = 100
-outputDir = r'C:\Users\zhelun\Desktop\aa'
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+fileType = config['DEFAULT']['fileType']
+outputDir = config['DEFAULT']['outputDir']
+numberOfFiles = config.getint('DEFAULT', 'numberOfFiles')
+
 
 # create directory
 def createDirectory(output_dir):
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
+
 # create a file in specified directory
-def createFile(output_dir):
-    with open(os.path.join(output_dir, generateFileName()), 'w') as dummyFile:
-        dummyFile.write(''.join(random.sample(string.ascii_letters + string.digits, random.randint(20, 40))))
+def createFile():
+    for i in range(numberOfFiles):
+        with open(os.path.join(outputDir, generateFileName()), 'w+') as dummyFile:
+            dummyFile.write(''.join(random.sample(string.ascii_letters + string.digits, random.randint(20, 40))))
+            dummyFile.close()
+            time.sleep(3)
 
 # generate non-repeat file name
 def generateFileName():
     localtime = time.strftime("%Y%m%d%H%M%S", time.localtime())
-    return 'dummy_file_%(filename)s.%(filetype)s'%{"filename": localtime, "filetype": fileType}
+    return 'dummy_file_%(filename)s.%(filetype)s' % {"filename": localtime, "filetype": fileType}
