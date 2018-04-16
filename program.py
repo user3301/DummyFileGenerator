fb1d9303c0c0de63a8d0cdcdc4b6d40144c51b14
CJ\zhelun <zhelun.gai@tmacomms.com>
2018-04-16 12:00:38 +1000

    test cases and starter program

---
+++ program.py
@@ -0,0 +1,4 @@
+from lib import createFile
+
+if __name__ == '__main__':
+    createFile()
--- tests/lib_test.py
+++ tests/lib_test.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from lib.core import*
+from lib.core import *
 
 import unittest
-
-
 
 
 class LibTestSuite(unittest.TestCase):
@@ -12,10 +10,11 @@ class LibTestSuite(unittest.TestCase):
 
     def test_generate_file_name(self):
         value = generateFileName()
-        self.assertEqual("dummy_file_%(time)s.mp3"%{"time":time.strftime("%Y%m%d%H%M%S", time.localtime())}, value)
+        self.assertEqual("dummy_file_%(time)s.mp3" % {"time": time.strftime("%Y%m%d%H%M%S", time.localtime())}, value)
 
     def test_create_file(self):
         createFile(outputDir)
+
 
 if __name__ == '__main__':
     unittest.main()
